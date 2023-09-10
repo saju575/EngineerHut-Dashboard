fb815a1dd3a2f093b95a08ef1f2656f7c9dcdccd
saju575 <sakilahmedsaju575@gmail.com>
2023-09-10 12:21:20 +0600

    initial setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/dashboard/navbar/Navbar";
+import Sidebar from "../components/dashboard/sidebar/Sidebar";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Sidebar />
+      </div>
+    </>
+  );
+};
+
+export default Main;
